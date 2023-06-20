7b0d8f322f83a7f60c3db4787b1adb044b6801bd
Lewis ushindi <coderflame3@gmail.com>
2023-06-20 23:35:03 +0300

    creating Nizzle header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-
+import NizzleNav from "../components/Nav";
 import Newsletter from "../components/NewsletterSub";
 import Footer from "../components/Footer";
 
 function Home(){
     return(
-        <section className="bg-gray-100">            
+        <section className="bg-gray-100"> 
+            <NizzleNav/>           
             <Newsletter/>
             <Footer/>
         </section>
