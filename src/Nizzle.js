fd93169ba6b64371031f6d86e5ba5f5843cc199b
Lewis ushindi <coderflame3@gmail.com>
2023-06-21 18:04:09 +0300

    Website coming soon

---
+++ src/Nizzle.js
@@ -0,0 +1,14 @@
+function handleToggle() {
+    var expandedMenu = document.getElementById("expandedMenu");
+    expandedMenu.classList.toggle("opacity-0");
+    expandedMenu.classList.toggle("opacity-100");
+    expandedMenu.classList.toggle("hidden"); // Add this line
+  }
+  
+  function handleLinkClick() {
+    var expandedMenu = document.getElementById("expandedMenu");
+    expandedMenu.classList.remove("opacity-100");
+    expandedMenu.classList.add("opacity-0");
+    expandedMenu.classList.add("hidden"); // Add this line
+  }
+  
