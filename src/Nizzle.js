99d28991bbf1378611ee314d781079e17a9c0ad4
Lewis ushindi <coderflame3@gmail.com>
2023-06-22 14:45:39 +0300

    update js

--- src/Nizzle.js
+++ src/Nizzle.js
@@ -11,4 +11,22 @@ function handleToggle() {
     expandedMenu.classList.add("opacity-0");
     expandedMenu.classList.add("hidden");
   }
+  function handleToggle() {
+    var expandedMenu = document.getElementById("expandedMenu");
+    expandedMenu.classList.toggle("opacity-0");
+    expandedMenu.classList.toggle("opacity-100");
+    expandedMenu.classList.toggle("hidden");
+  }
   
+  function handleLinkClick() {
+    var expandedMenu = document.getElementById("expandedMenu");
+    expandedMenu.classList.remove("opacity-100");
+    expandedMenu.classList.add("opacity-0");
+    expandedMenu.classList.add("hidden");
+  }
+
+  document.getElementById("viewProductsBtn").addEventListener("click", function(event) {
+    event.preventDefault();
+    const targetSection = document.getElementById("Products");
+    targetSection.scrollIntoView({ behavior: "smooth" });
+  });
